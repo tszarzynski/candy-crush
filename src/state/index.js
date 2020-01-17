20018dc86b6073a5e980c154f538477d46d1eabb
Tomasz Szarzynski <tszarzynski@gmail.com>
2020-01-17 21:29:09 +0000

    Implemented game end scenario

--- src/state/index.js
+++ src/state/index.js
@@ -7,11 +7,14 @@ export const Actions = {
 const availableColors = ["red", "green", "blue", "yellow"];
 const gridSize = 10;
 
-export const initialState = {
-  grid: Array.from(
+const makeGrid = (gridSize, availableColors) =>
+  Array.from(
     { length: gridSize * gridSize },
     () => availableColors[randomNum(0, availableColors.length - 1)]
-  )
+  );
+
+export const initialState = {
+  grid: makeGrid(gridSize, availableColors)
 };
 
 export const reducer = (state, action) => {
@@ -28,6 +31,17 @@ export const reducer = (state, action) => {
       }
 
       const newGrid = dropItems(state.grid, gridSize, neighbours);
+
+      // check if grid is empty or no more blocks can be removed
+      // rebuild grid if necessary
+      if (
+        newGrid.filter(gridItem => gridItem != null).length === 0 ||
+        newGrid.reduce((acc, gridItem, i) => {
+          if (gridItem === null) return acc;
+          else return acc + findNeighbours(newGrid, gridSize, i).length - 1;
+        }, 0) === 0
+      )
+        return { ...state, grid: makeGrid(gridSize, availableColors) };
 
       return { ...state, grid: newGrid };
 
